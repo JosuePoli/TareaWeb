70dc5cf9953f432011359df14b3b609a94fce12e
JosuePoli <der.poli127@hotmail.com>
2018-10-12 15:53:50 -0500

    oxxo

---
+++ mat2/js/app.js
@@ -0,0 +1,10 @@
+ /*Animacion del navbar*/ 
+
+
+
+
+  $(document).ready(function(){ /*Esta linea hace las acciones*/
+    $('.parallax').parallax();  /*Esta es la accion*/
+    $(".dropdown-trigger").dropdown();
+  });
+        
